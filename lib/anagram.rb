214af6eaff2b78742ef187d3680edadc75cf22f5
Erin Ponsonby <erinponsonby@gmail.com>
2019-05-13 14:10:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,6 @@ class Anagram
   end
   
   def match(array_of_words)
-    empty_array = []
     matches = []
     split_and_sorted_instance_variable = @word.split("").sort
     array_of_words.each_with_index do |word, index|
