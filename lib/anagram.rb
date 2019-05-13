b09054f2350b6cfc8b41f4d441ed202d1e8befb4
Erin Ponsonby <erinponsonby@gmail.com>
2019-05-13 14:08:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -15,7 +15,7 @@ class Anagram
         new_matches << array_of_words.delete_if{|word| word != array_of_words[index]}
       end
     end
-      if new_matches.length < 0
+      if new_matches.length > 0
           return new_matches
       else
           return empty_array
