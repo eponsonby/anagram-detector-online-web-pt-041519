0ae189f30613f7cb56709f0f81efad19cc7d082f
Erin Ponsonby <erinponsonby@gmail.com>
2019-05-13 14:07:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,14 +8,20 @@ class Anagram
   
   def match(array_of_words)
     empty_array = []
+    new_matches = []
     split_and_sorted_instance_variable = @word.split("").sort
     array_of_words.each_with_index do |word, index|
       if split_and_sorted_instance_variable == word.split("").sort
-        return array_of_words.delete_if{|word| word != array_of_words[index]}
+        new_matches << array_of_words.delete_if{|word| word != array_of_words[index]}
       end
     end
-      return empty_array
-    end
+      if new_matches.length < 0
+          return new_matches
+      else
+          return empty_array
+      end
+  end  
+end
 
 
 end
